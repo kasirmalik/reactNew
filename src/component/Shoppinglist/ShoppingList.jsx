eb975a7a5373da990b0946c6bde2ffd15f6688e2
Kaiser Fayaz <kasirmalik87@gmail.com>
2025-05-31 21:51:13 +0530

    feat: implement ShoppingList component with input handling and state management

--- src/component/Shoppinglist/ShoppingList.jsx
+++ src/component/Shoppinglist/ShoppingList.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import "./shopping.css";
 
 function ShoppingList() {
+  const [food, setfood] = useState("");
+  const handleInput = (e) => {
+    console.log(e.target.value);
+    setfood(e.target.value);
+  };
+  useEffect(() => {
+    
+  
+  }, []);
+
   return (
-    <div>
-      
+    <div className="shop">
+      <h1>My Shopping List</h1>
+      <div>
+        <input type="text" value={food} onChange={handleInput} />
+      </div>
+      <div className="shopping-list">{food}</div>
+      <div className="bucket"></div>
     </div>
-  )
+  );
 }
 
-export default ShoppingList
+export default ShoppingList;
